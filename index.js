2bfb0601c522c42674a580539e02ebbb8ab76bb6
fermartinotti <fermartinotti@gmail.com>
2021-08-19 12:33:40 -0300

    se agrega llamado a la API para obtener el precio del dolar

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require('dotenv').config();
+const https = require('https');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -23,11 +24,41 @@ client.on("message", function(message) {
     command = args.shift().toLowerCase();
 
     if (command === "dolar") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(` Subio de nuevo `);
+        message.reply(` Subio de nuevo `); 
     }
   
 });
 
+client.on("ready", function(message) {
+    obtenerPrecioDolarBlue();
+    
+})
 
 client.login(token);
+
+function obtenerPrecioDolarBlue(){
+    const options = {
+        hostname: 'api-dolar-argentina.herokuapp.com',
+        port:443,
+        path: '/api/dolarblue',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+    }
+    
+    const req = https.request(options, res => {        
+        res.on('data', d => {
+            var reqBody = d.toString();
+            reqBody = JSON.parse(reqBody);
+            client.user.setActivity(`Precio: ${reqBody.venta}`, {type: 'WATCHING'});
+        })
+    })
+      
+    req.on('error', error => {
+        console.error(error);
+    })
+
+    req.end();
+
+}
